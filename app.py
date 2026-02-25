3261ed51cf89e6f94602348c28e18ba6bb1e0ff2
simran agrawal <simranagrawal3636@gmail.com>
2026-02-25 23:31:46 +0530

    updated fronted text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Simran's DevOps Project!"
+    return "Hello from Simran's first end-to-end DevOps Project!"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
